a6755a2c149741bb262cb29a2b47a2a776d40074
Aaron <nordin.aaron@gmail.com>
2019-07-19 17:57:18 -0600

    Input styling

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,4 +1,93 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import {
+  fade,
+  withStyles,
+  makeStyles,
+  createMuiTheme
+} from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import InputBase from "@material-ui/core/InputBase";
+import InputLabel from "@material-ui/core/InputLabel";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import { green } from "@material-ui/core/colors";
+import "./UserInput.css";
+
+const CssTextField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "green"
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "green"
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "red"
+      },
+      "&:hover fieldset": {
+        borderColor: "yellow"
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "green"
+      }
+    }
+  }
+})(TextField);
+
+const BootstrapInput = withStyles(theme => ({
+  root: {
+    "label + &": {
+      marginTop: theme.spacing(3)
+    }
+  },
+  input: {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.common.white,
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    width: "auto",
+    padding: "10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(","),
+    "&:focus": {
+      boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main
+    }
+  }
+}))(InputBase);
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  margin: {
+    margin: theme.spacing(1)
+  }
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green
+  }
+});
 
 export default class UserInput extends Component {
   constructor() {
@@ -47,27 +136,49 @@ export default class UserInput extends Component {
   }
 
   render() {
+    const classes = useStyles;
     return (
       <div className="userIn">
-        <div className="userIns">
-          <input
-            className="userIn-name"
-            type="text"
-            onChange={e => this.handleName(e.target.value)}
-            placeholder="Enter Gene Name"
-          />
-          <input
-            className="userIn-dna"
-            type="text"
-            onChange={e => this.handleDNA(e.target.value)}
-            placeholder="Enter DNA sequence"
-          />
+        <div className={classes.root}>
+          <ThemeProvider theme={theme}>
+            <TextField
+              className={classes.margin}
+              label="Enter Gene Name"
+              variant="outlined"
+              id="mui-theme-provider-outlined-input"
+              onChange={e => this.handleName(e.target.value)}
+            />
+            <TextField
+              className={classes.margin}
+              label="Enter DNA Sequence"
+              variant="outlined"
+              id="mui-theme-provider-outlined-input"
+              onChange={e => this.handleDNA(e.target.value)}
+            />
+          </ThemeProvider>
         </div>
-        <div className="userIn-buttons">
-          <button onClick={() => this.handleTScriptClick()}>Transcribe DNA</button>
-          <button onClick={() => this.handleTLateClick()}>Translate DNA</button>
-          <button onClick={() => this.handleBothClick()}>Both</button>
-        </div>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Grid container spacing={1} direction="column" alignItems="center">
+              <Grid item>
+                <ButtonGroup
+                  id="tri-button"
+                  variant="contained"
+                  color="primary"
+                  aria-label="Full-width contained primary button group"
+                >
+                  <Button onClick={() => this.handleTScriptClick()}>
+                    Transcribe DNA
+                  </Button>
+                  <Button onClick={() => this.handleTLateClick()}>
+                    Translate DNA
+                  </Button>
+                  <Button onClick={() => this.handleBothClick()}>Both</Button>
+                </ButtonGroup>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     );
   }
